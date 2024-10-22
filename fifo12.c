35dbab6341c93d9944f57364f449855d3573e2b0
hak4ry <167526173+hak4ry@users.noreply.github.com>
2024-10-22 13:35:02 +0200

    Create fifo12.c

---
+++ fifo12.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define FIFO1 "/tmp/fifo1"
+#define FIFO2 "/tmp/fifo2"
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    int num, result;
+    int fd1, fd2;
+
+    fd1 = open(FIFO1, O_RDONLY);
+    read(fd1, &num, sizeof(int));
+    close(fd1);
+   
+    result = factorial(num);
+    printf("fifo12: Factorial calculado de %d es %d\n", num, result);
+  
+    fd2 = open(FIFO2, O_WRONLY);
+    write(fd2, &result, sizeof(int));
+    close(fd2);
+
+    return 0;
+}
